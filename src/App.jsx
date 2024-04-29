99352d04881b12b795aa6ade8265813ea847a73e
Asad Haider <asad.haider@techlogix.com>
2024-04-29 23:58:20 +0500

    Configured Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@
 function App() {
 
   return (
-    <>
-      <h1>Mecoder</h1>
-    </>
+    <div className="h-screen overflow-hidden md:h-auto md:overflow-auto">
+      <div className='flex justify-center py-4'>
+        <h1 className="text-center text-primary font-bold font-redHatDisplay text-[30px] md:text-[45px]">Mecoder</h1>
+      </div>
+
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4B5EC1',
+        secondary: '#000000'
+      },
+      fontFamily: {
+        redHatDisplay: ["Red Hat Display", "sans-serif"]
+      }
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px"
+    }
   },
   plugins: [],
 }
